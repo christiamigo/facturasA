fb317f00b28f077d2acae3ed6bcfea01b0b326bb
Martin <martinsanchezavila@gmail.com>
2020-05-20 17:49:26 +0200

    Implementacion de la interfaz FacturasI.java en Factura.java

--- Facturas/src/Factura.java
+++ Facturas/src/Factura.java
@@ -1,4 +1,4 @@
-public class Factura {
+public class Factura implements FacturasI {
 		public int num;
 		public float base;
 		public float tipoIva;
@@ -7,4 +7,11 @@ public class Factura {
 	public static void main(String[] args) {
 
 	}
+
+	public void cambiarEstado(Estados estado){
+}
+	public float getTotal(){
+}
+	public float getCantidadIva(){
+}
 }
