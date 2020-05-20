fdcc0cdfe4b591bd18c05cdffa92f3ac628c7ce1
Martin <martinsanchezavila@gmail.com>
2020-05-20 17:41:37 +0200

    Descomentar atributo Factura.java

--- Facturas/src/Factura.java
+++ Facturas/src/Factura.java
@@ -2,9 +2,9 @@ public class Factura {
 		public int num;
 		public float base;
 		public float tipoIva;
-		//public Estados estado;
-		
+		public Estados estado;
+
 	public static void main(String[] args) {
-	
+
 	}
 }
